1ec00b0e79de45f481405e9ed5b08557fe5d1b1c
Renanvt <jonatan_rvs@hotmail.com>
2021-08-13 15:27:35 -0300

    finish layout home

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include 'layout/header.php';
+?>
+        <aside>
+            <div id="rect-fundo">
+            </div>
+            <div id="rect-frente">
+                <img src="img/section/800px-Brigadeiro.jpg" alt="Foto-de-brigadeiro-de-chocolate" id="brigadeiro" />
+
+                <img src="img/section/paçoca.jpg" alt="Foto-de-brigadeiro-de-paçoca" id="pacoca" />
+
+
+                <img src="img/section/beijinho.jpg" alt="Foto-de-brigadeiro-de-beijinho" id="beijinho" />
+            </div>
+        </aside>
+
+        <main>
+            <div id="main-rect-fundo">
+                <div id="main-rect-frente">
+
+                </div>
+            </div>
+
+        </main>
+<?php
+
+include 'layout/footer.php';
+?>
---
+++ layout/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lucky Sugar</title>
+    <link  href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <main id="corpo">
+        <header id="rect-main">
+            <nav id="menu">
+                <ul>
+                    <li>
+                        <div class="rect" id="Home">
+                            <a href="index.php">Home</a>
+                        </div>
+                        <div class="rect" id="Produtos"><a href="index.php">Produtos</a></div>
+                        <div class="rect" id="QuemSomos">
+                        <a href="index.php">Quem somos</a></div>
+                        <div class=" rect" id="Contato"><a href="index.php">Contato</a></div>
+                        <div class="rect" id="Localização"><a href="index.php">Localização</a></div>
+                    </li>
+                </ul>
+            </nav>
+        </header>
