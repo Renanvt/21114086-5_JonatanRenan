b2d0f3e83eba66fffc9b0c4b8bb669c6b3496b0e
Renanvt <jonatan_rvs@hotmail.com>
2021-08-13 16:44:44 -0300

    adding content and css style

--- index.php
+++ index.php
@@ -1,27 +1,60 @@
 <?php
 include 'layout/header.php';
 ?>
-        <aside>
-            <div id="rect-fundo">
-            </div>
-            <div id="rect-frente">
-                <img src="img/section/800px-Brigadeiro.jpg" alt="Foto-de-brigadeiro-de-chocolate" id="brigadeiro" />
+<aside>
+    <div id="rect-fundo">
+    </div>
+    <div id="rect-frente">
+        <img src="img/section/800px-Brigadeiro.jpg" alt="Foto-de-brigadeiro-de-chocolate" id="brigadeiro" />
 
-                <img src="img/section/paçoca.jpg" alt="Foto-de-brigadeiro-de-paçoca" id="pacoca" />
+        <img src="img/section/paçoca.jpg" alt="Foto-de-brigadeiro-de-paçoca" id="pacoca" />
 
 
-                <img src="img/section/beijinho.jpg" alt="Foto-de-brigadeiro-de-beijinho" id="beijinho" />
-            </div>
-        </aside>
+        <img src="img/section/beijinho.jpg" alt="Foto-de-brigadeiro-de-beijinho" id="beijinho" />
+    </div>
+</aside>
 
-        <main>
-            <div id="main-rect-fundo">
-                <div id="main-rect-frente">
+<main>
+    <div id="main-rect-fundo">
+        <div id="main-rect-frente">
+            <section>
+                <header>
+                    <h1>Quem somos</h1>
+                </header>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore voluptas recusandae explicabo beatae sequi quidem, quibusdam in asperiores mollitia, quaerat at maxime sit inventore. Odio quisquam quam eveniet eaque ipsum?</p>
+                <button type="submit" value="Ver mais"><span>Ver mais</span></button>
+            </section>
 
-                </div>
-            </div>
+            <section>
+                <header>
+                    <h1>Onde estamos localizados</h1>
+                </header>
 
-        </main>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore voluptas recusandae explicabo beatae sequi quidem, quibusdam in asperiores mollitia, quaerat at maxime sit inventore. Odio quisquam quam eveniet eaque ipsum?
+                </p>
+                <!--
+                <section id="mapa">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.5313727766566!2d-51.91905498457065!3d-23.441289884742663!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94e81a413a9c9463%3A0x4e3ee356ad1bd6c3!2sUniCesumar%20-%20Maring%C3%A1!5e0!3m2!1sen!2sbr!4v1628879658720!5m2!1sen!2sbr" width="152" height="125" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </section>
+                <-->
+                <button type="submit" value="Ver mais"><span>Ver mais</span></button>
+
+            </section>
+
+            <section>
+                <header>
+                    <h1>Contato</h1>
+                </header>
+
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore voluptas recusandae explicabo beatae sequi quidem, quibusdam in asperiores mollitia, quaerat at maxime sit inventore. Odio quisquam quam eveniet eaque ipsum?</p>
+
+                <button type="submit" value="Ver mais"><span>Ver mais</span></button>
+
+            </section>
+        </div>
+    </div>
+
+</main>
 <?php
 
 include 'layout/footer.php';
